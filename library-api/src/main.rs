04f15501d2f8792fc2492f8e6795bca2c2de9898
Luigi Foscari <luigi.foscari@icloud.com>
2022-07-07 11:56:54 +0200

    removed actix, refactoring

--- library-api/src/main.rs
+++ library-api/src/main.rs
@@ -1,18 +1,26 @@
 extern crate redis;
+
 use redis::Commands;
+use std::env;
 
-fn main() -> redis::RedisResult<()> {
-    let db_address  = env!("REDIS_ENDPOINT");
-    let db_username = env!("REDIS_USERNAME");
-    let db_password = env!("REDIS_PASSWORD");
-
-    let connection_url = format!("redis://{db_username}:{db_password}@{db_address}");
+fn connect_library(address: String, username: String, password: String) -> redis::RedisResult<redis::Connection> {
+    let connection_url = format!("redis://{username}:{password}@{address}");
 
     let client = redis::Client::open(connection_url)?;
-    let mut connection = client.get_connection()?;
+    let connection = client.get_connection()?;
 
-    let _: () = connection.set("MICH", "FLIX")?;
-    let flix: String = connection.get("MICH")?;
+    Ok(connection)
+}
+
+fn main() -> redis::RedisResult<()> {
+    let db_address  = env::var("REDIS_ENDPOINT").unwrap();
+    let db_username = env::var("REDIS_USERNAME").unwrap();
+    let db_password = env::var("REDIS_PASSWORD").unwrap();
+
+    let mut library = connect_library(db_address, db_username, db_password)?;
+
+    let _: () = library.set("MICH", "FLIX")?;
+    let flix: String = library.get("MICH")?;
 
     println!("MICH{}", flix);
 
